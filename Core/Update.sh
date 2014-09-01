8e9624812d35365bb054704a4080466fdf0df65d
Damian Majchrzak <DamiaX@users.noreply.github.com>
2014-09-01 12:19:30 +0200

    Update Update.sh

--- Core/Update.sh
+++ Core/Update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Copyright © 2014 Damian Majchrzak (DamiaX)
 #http://damiax.github.io/kernelup/
-url="https://raw.githubusercontent.com/DamiaX/YT_Playlist_downloader/master/Core/yt-download.sh";
+url="https://raw.githubusercontent.com/DamiaX/YT_Playlist_downloader/master/yt-download.sh";
 remove_url="https://raw.githubusercontent.com/DamiaX/YT_Playlist_downloader/master/Core/remove.sh"
 name="yt-download";
 remove_name="remove.sh"
