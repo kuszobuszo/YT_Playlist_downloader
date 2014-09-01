70d1fe494b27db6539b3edd217a6935ee0eb1ced
Damian Majchrzak <DamiaX@users.noreply.github.com>
2014-09-01 11:33:36 +0200

    Create remove.sh

---
+++ Core/remove.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Copyright © 2014 Damian Majchrzak (DamiaX)
+#http://damiax.github.io/YT_Playlist_downloader/
+
+
+sudo rm -rf /usr/local/bin/yt-download*; 
+sudo rm -rf yt-download*; 
+
+sudo rm -rf $0;
+
+exit;
